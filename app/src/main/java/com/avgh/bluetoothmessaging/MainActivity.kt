65e8a2c489aff6c289bc807bb72448a0a205ab19
shulkmaster <00088116@uca.edu.sv>
2019-06-18 17:31:02 -0600

    agregando elementos de la interfaz

--- app/src/main/java/com/avgh/bluetoothmessaging/MainActivity.kt
+++ app/src/main/java/com/avgh/bluetoothmessaging/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.avgh.bluetoothmessaging
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.avgh.bluetoothmessaging.server.ServerAdmin
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.test).setOnClickListener {
+            startActivity(Intent(this, ServerAdmin::class.java))
+        }
     }
 }
