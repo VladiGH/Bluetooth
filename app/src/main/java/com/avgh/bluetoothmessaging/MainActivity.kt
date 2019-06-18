431a167609cff9ffec6e0ffdc50308082ad3927a
Andre <00103017@uca.edu.sv>
2019-06-18 07:46:02 -0600

    first commit

---
+++ app/src/main/java/com/avgh/bluetoothmessaging/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.avgh.bluetoothmessaging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
