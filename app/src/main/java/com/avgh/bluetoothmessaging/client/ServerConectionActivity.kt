c881ddc543e4bafbc2ce78e9580a247e8d2daa4e
Alex7498-SV <00068817@uca.edu.sv>
2019-06-18 22:07:34 -0600

    avances en la UI del cliente

---
+++ app/src/main/java/com/avgh/bluetoothmessaging/client/ServerConectionActivity.kt
@@ -0,0 +1,12 @@
+package com.avgh.bluetoothmessaging.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ServerConectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_server_conection)
+    }
+}
