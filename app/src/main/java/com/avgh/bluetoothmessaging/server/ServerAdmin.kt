1e7940602fb966b4e63d1308cbe24a23db66275e
shulkmaster <00088116@uca.edu.sv>
2019-06-18 21:23:49 -0600

    primera version de la interfaz

--- app/src/main/java/com/avgh/bluetoothmessaging/server/ServerAdmin.kt
+++ app/src/main/java/com/avgh/bluetoothmessaging/server/ServerAdmin.kt
@@ -1,7 +1,12 @@
 package com.avgh.bluetoothmessaging.server
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import android.widget.ToggleButton
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.graphics.drawable.DrawableCompat
 import com.avgh.bluetoothmessaging.R
 
 class ServerAdmin : AppCompatActivity() {
@@ -9,5 +14,22 @@ class ServerAdmin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_server_admin)
+        findViewById<ToggleButton>(R.id.activator).setOnCheckedChangeListener { buttonView, isChecked ->
+            changeAnimation(buttonView, isChecked)
+        }
     }
+
+    private fun changeAnimation(button: CompoundButton, isOn: Boolean) {
+        val color = if (isOn)
+            Color.rgb(3, 205, 203)
+        else Color.rgb(0, 0, 0)
+        val newDraw = AppCompatResources.getDrawable(this, R.drawable.ic_hexagon)
+        val wrappedDraw = newDraw?.let { DrawableCompat.wrap(it) }
+        if (wrappedDraw != null) {
+            DrawableCompat.setTint(wrappedDraw, color)
+            button.background = wrappedDraw
+        }
+
+    }
+
 }
