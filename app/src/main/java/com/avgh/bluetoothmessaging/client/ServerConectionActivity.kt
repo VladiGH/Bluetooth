0e4c1fc8714a559d4c1c54a566ac4328edf55eae
Alex7498-SV <00068817@uca.edu.sv>
2019-06-19 08:51:04 -0600

    Configuracion de la UI del cliente

--- app/src/main/java/com/avgh/bluetoothmessaging/client/ServerConectionActivity.kt
+++ app/src/main/java/com/avgh/bluetoothmessaging/client/ServerConectionActivity.kt
@@ -1,12 +1,34 @@
 package com.avgh.bluetoothmessaging.client
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.avgh.bluetoothmessaging.R
+import com.google.android.material.snackbar.Snackbar
 
 class ServerConectionActivity : AppCompatActivity() {
+
+
+
+    var bTAdapter : BluetoothAdapter? = null
+    lateinit var  devices : Set<BluetoothDevice>
+    var R_ENABLE_BLUETOOTH = 1
+
+    companion object{
+        val EXTRA_ADDRESS : String = "D_address"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_server_conection)
+
+        bTAdapter = BluetoothAdapter.getDefaultAdapter()
+
+        if(bTAdapter == null){
+            Toast.makeText(this,  "Su dispositivo no cuenta con tecnologia Bluetooth", Toast.LENGTH_LONG).show()
+        }
+        else if(!bTAdapter!!)
     }
 }
