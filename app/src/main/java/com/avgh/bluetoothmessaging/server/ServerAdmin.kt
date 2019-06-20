74bd5ea9d2ea9ab27447c7a481b0b8e378f61c76
shulkmaster <00088116@uca.edu.sv>
2019-06-19 23:33:13 -0600

    a saber que pex

--- app/src/main/java/com/avgh/bluetoothmessaging/server/ServerAdmin.kt
+++ app/src/main/java/com/avgh/bluetoothmessaging/server/ServerAdmin.kt
@@ -3,30 +3,58 @@ package com.avgh.bluetoothmessaging.server
 import android.bluetooth.BluetoothAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.ToggleButton
+import android.content.Intent
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.*
+import android.bluetooth.BluetoothSocket
+import android.util.Log
+import java.io.IOException
 import com.avgh.bluetoothmessaging.R
+
 
 class ServerAdmin : AppCompatActivity() {
 
     private val adapter = BluetoothAdapter.getDefaultAdapter()
+    private val requestBt = 8
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_server_admin)
-
         findViewById<ToggleButton>(R.id.activator)
             .setOnCheckedChangeListener { _, isChecked ->
                 startServer(isChecked)
             }
-
     }
 
     private fun startServer(status: Boolean) {
-        if (status) adapter.enable() else adapter.disable()
-        val pairedDevices = adapter.bondedDevices
-        pairedDevices.forEach {
-            Log.d("ServerBlue", it.name)
+        if (status) {
+            if (!adapter.isEnabled) {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, requestBt)
+            }
+            GlobalScope.launch {
+                val connexion = adapter
+                    .listenUsingRfcommWithServiceRecord(
+                        "pancho",
+                        UUID.fromString("3ceee1db-d30d-41f8-8379-5acde949b6f9")
+                    )
+                var socket: BluetoothSocket? = null
+                while (true) {
+                    try {
+                        socket = connexion.accept()
+                    } catch (e: IOException) {
+                        Log.d("Pancho", "error capaz", e)
+                        break
+                    }
+                    if (socket != null) {
+                        val xd = socket.inputStream.readBytes().toString()
+                        Log.d("Pancho", "mensaje xd: $xd")
+                        break
+                    }
+                }
+            }
         }
     }
 
