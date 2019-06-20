e5392fe80a8434357f424d58a6b274fa0a257f8b
RodrigoCruz1154 <00115417@uca.edu.sv>
2019-06-19 21:32:41 -0600

    se mandó a llamar comprobarBT

--- app/src/main/java/com/avgh/bluetoothmessaging/client/ServerConectionActivity.kt
+++ app/src/main/java/com/avgh/bluetoothmessaging/client/ServerConectionActivity.kt
@@ -24,7 +24,7 @@ class ServerConectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_server_conection)
-
+        comprobarBT()
 
     }
 
